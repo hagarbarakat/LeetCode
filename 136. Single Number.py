00f356fbeb13c612397debc2695399cef462553b
Hagar Barakat <hagarbarakat@hotmail.com>
2020-04-01 18:02:40 +0200

    30-Day LeetCoding Challenge

---
+++ 136. Single Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0 
+        for i in range(len(nums)):
+            ans = ans ^ nums[i]
+        return ans
