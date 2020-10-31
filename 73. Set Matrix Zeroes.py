4f3ba05a579ce80b54749905b098193cdbd28e76
Hagar Barakat <hagarbarakat@hotmail.com>
2020-10-31 22:18:58 +0200

    Update 73. Set Matrix Zeroes.py

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
         is_col = False
         R = len(matrix)
         C = len(matrix[0])
